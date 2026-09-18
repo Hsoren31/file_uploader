26b6ec7d8dd5450ddf6a7b84203b7ce7605614cf
Hsoren31 <sorensenhaley@gmail.com>
2026-09-18 14:55:49 -0600

    FIX: add environment variable for app port

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get("/", async (req, res) => {
   res.render("index", { folders, files });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}`);
 });
